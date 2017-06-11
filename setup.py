7e8e97aaf6690b897e1c704914165b9a72441cb3
Morgan Wahl <morgan@morgan.wahl.name>
2017-06-11 02:34:56 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='base_private',
+    version='0.1',
+    description='Encode arbitrary bits in private-use codepoints.',
+    url='https://github.com/morganwahl/base-private-use',
+    author='Morgan Wahl',
+    author_email='morgan.wahl@gmail.com',
+    license='GPLv3',
+)
